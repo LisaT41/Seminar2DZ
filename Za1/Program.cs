d7ece10fd1c049260de9469a2775ff3aed602b24
l.gor41000@yandex.ru <l.gor41000@yandex.ru>
2023-04-02 08:14:27 +0200

    first task

---
+++ Za1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа. Не использовать строки для расчета.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int ReadInt(string message)
+{
+    System.Console.Write($"{message} > ");
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int number = ReadInt("enter number");
+int lastDigit = number / 10 % 10;
+System.Console.WriteLine($"Last chiffre of number {number} egal {lastDigit}");
---
+++ Za3/Program.cs
@@ -0,0 +1,49 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+//6  -> да 
+// 7 -> да 
+// 1 -> нет
+
+var Monday = "1";
+var Tuesday = "2";
+var Wensday = "3";
+var Thursday = "4";
+var Friday = "5";
+var Saterday = "6";
+var Sunday = "7";
+
+System.Console.WriteLine("Enter number of day");
+var input = int.Parse(Console.ReadLine());
+
+if (input == 1)
+{
+    Console.WriteLine("No");
+}
+if (input == 2)
+{
+    Console.WriteLine("No");
+}
+if (input == 3)
+{
+    Console.WriteLine("No");
+}
+if (input == 4)
+{
+    Console.WriteLine("No");
+}
+if (input == 5)
+{
+    Console.WriteLine("No");
+}
+if (input == 6)
+{
+    Console.WriteLine("Yes");
+}
+if (input == 7)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("Your number not correctly");
+}
